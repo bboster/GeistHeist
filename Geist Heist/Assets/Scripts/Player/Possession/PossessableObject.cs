a1dd87a33a5f58f3e4c9613613528e91eee5f374
SkyBeal <thesnugpug1@gmail.com>
2025-09-18 22:02:52 -0500

    finished possession

--- Geist Heist/Assets/Scripts/Player/Possession/PossessableObject.cs
+++ Geist Heist/Assets/Scripts/Player/Possession/PossessableObject.cs
@@ -1,53 +1,21 @@
 /*
- * Contributors: Toby
+ * Contributors: Toby, Sky
  * Creation Date: 9/16/25
- * Last Modified: 9/16/25
+ * Last Modified: 9/18/25
  * 
  * Brief Description: On every possessable object, and the player for simplicity. 
  * Contains reference to input scripts and other stuff.
  *  
- *  TODO: this should probably need a reference to the camera and an anchor point (for 3rd person).
- *  Also a reference to the thing the camera is orbiting
+ *  TODO:
  */
-
-
-
-//change cameras by turning cameras on and off
-//use triggers to detect when ghost is close enough, on button press
-//not detectable, can't move
-//press button to perform an action (spawn a cube or something)
 using UnityEngine;
+using Unity.Cinemachine;
+using NaughtyAttributes;
 
 public class PossessableObject : MonoBehaviour
 {
     public IInputHandler InputHandler;
-
-    //oncontrol()
-    //possess()
-
-    public void Possess()
-    {
-        SwitchCamera();
-        //not detectable
-        //disable movement
-        //can do thing
-    }
-
-    public void SwitchCamera()
-    {
-        //switch from 1st to 3rd
-    }
-
-    public void ChangeMovement()
-    {
-        //disable movement
-        //or change movement to specific object
-    }
-
-    public void PerformAction()
-    {
-        //on button push, do action if applicable
-    }
+    [Required] public CinemachineCamera CinemachineCamera;
 
     private void Awake()
     {
