0e83ded94f8ec2447e8419398e864bb0deeac5e3
Joseph Consoli <114947767+JRavioli@users.noreply.github.com>
2025-10-01 17:24:20 -0500

    Pushed changes before I send laptop for repairs

---
+++ Geist Heist/Assets/Scripts/Audio/TestLoopSFX.cs
@@ -0,0 +1,19 @@
+using FMOD.Studio;
+using UnityEngine;
+
+public class TestLoopSFX : MonoBehaviour
+{
+    EventInstance TestSFX;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
