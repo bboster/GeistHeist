c95e4a90a0e32735a2c95b64631bcea1839149d9
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-10-01 17:53:31 -0500

    Collectable Testing scene and mid shader

--- Geist Heist/Assets/Scripts/Environment/OptionalCollectable.cs
+++ Geist Heist/Assets/Scripts/Environment/OptionalCollectable.cs
@@ -1,3 +1,14 @@
+/*
+ * Contributors: Toby
+ * Creation: 9/30/25
+ * Last Edited: 9/30/25
+ * Summary: Collectable object. saves to player data.
+ */
+
+using NaughtyAttributes;
+using System;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class OptionalCollectable : MonoBehaviour
@@ -13,4 +24,42 @@ public class OptionalCollectable : MonoBehaviour
     {
         
     }
+
+    [Button("Add new collectable as enum")]
+    private void RegisterAsEnum()
+    {
+        var currentEnums = Enum.GetNames(typeof(Collectable));
+        var name = this.gameObject.name;
+        foreach (var s in currentEnums)
+        {
+            Debug.Log(s);
+        }
+        if (currentEnums.Contains(name))
+        {
+            Debug.Log("Enum already exists");
+            return;
+        }
+
+        // Get the path to this script file
+        string scriptPath = Application.dataPath + "/OptionalCollectable.cs";
+
+        // Read the current content of the script
+        string currentContent = File.ReadAllText(scriptPath);
+
+        // Modify the content (e.g., add a comment)
+        string newContent = currentContent + "\n// This line was added programmatically.\n";
+
+        // Write the new content back to the file, overwriting the old content
+        File.WriteAllText(scriptPath, newContent);
+
+        Debug.Log("Script modified and saved.");
+    }
+}
+
+
+
+public enum Collectable
+{
+    TestName1,
+    TestName2,
 }
---
+++ Geist Heist/Assets/Scripts/Save Data/SaveData.cs
@@ -0,0 +1,17 @@
+/*
+ * Contributors: Toby
+ * Creation: 9/30/25
+ * Last Edited: 9/30/25
+ * Summary: The players save file.
+ * 
+ * TODO: actually save and load
+ */
+
+
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData
+{
+    public HashSet<string> ScenesCompleted;
+}
