223022f371ab7f802d72d1263bf1b79abdb26dfa
JBBateman <jbateman2@mail.bradley.edu>
2025-10-07 11:12:53 -0500

    VendingMachineSoundStimulus

--- Geist Heist/Assets/Scripts/Guards/Pathing & Detection/SoundStimulus.cs
+++ Geist Heist/Assets/Scripts/Guards/Pathing & Detection/SoundStimulus.cs
@@ -2,16 +2,24 @@
  * Author: Jacob Bateman
  * Contributors:
  * Creation: 10/02/25
- * Last Edited: 10/02/25
+ * Last Edited: 10/07/25
  * Summary: The stimulus trigger for a sound or distractable-based stimulus.
  */
 
+using System.Collections;
 using UnityEngine;
 
 
 public class SoundStimulus : Stimulus
 {
     private GuardController contactedGuard;
+
+    [SerializeField] private float soundLifetime;
+
+    private void Start()
+    {
+        StartCoroutine(SoundLength());
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -28,5 +36,15 @@ public class SoundStimulus : Stimulus
     public override void TriggerStimulus()
     {
         contactedGuard.RecieveStimulus(this, stateToChangeTo, transform.position);
+    }
+
+    /// <summary>
+    /// Controls the lifetime of the sound.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator SoundLength()
+    {
+        yield return new WaitForSeconds(soundLifetime);
+        Destroy(gameObject);
     }
 }
