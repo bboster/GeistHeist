ba6cc0a15e9d34c1e2c22c89682e83af51072236
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-10-09 13:14:52 -0500

    Billboard UI Testing

---
+++ Geist Heist/Assets/Scripts/UI/IBillboardUI.cs
@@ -0,0 +1,18 @@
+/*
+ * Contributors:  Toby
+ * Creation Date: 10/9/25
+ * Last Modified: 10/9/25
+ * 
+ * Brief Description: Interface for Billboard UI objects.
+ * Billboard ui objects do the following:
+ * - stays in a single world point, 
+ * - remains a consistent size, 
+ * - always faces the player
+ */
+
+using UnityEngine;
+
+public abstract class IBillboardUI : MonoBehaviour
+{
+    
+}
