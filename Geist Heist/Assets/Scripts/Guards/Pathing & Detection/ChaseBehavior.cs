8d524006c670c515938efe08948fb54c9a372367
JBBateman <jbateman2@mail.bradley.edu>
2025-10-28 10:00:17 -0500

    Guard will continue to chase player when they are in a possessable

--- Geist Heist/Assets/Scripts/Guards/Pathing & Detection/ChaseBehavior.cs
+++ Geist Heist/Assets/Scripts/Guards/Pathing & Detection/ChaseBehavior.cs
@@ -21,13 +21,13 @@ public class ChaseBehavior : GuardMovement
     public override void InitializeBehavior(GameObject selfRef)
     {
         base.InitializeBehavior(selfRef);
-        ThirdPersonInputHandler.OnPossessObject += selfRef.GetComponent<GuardController>().ChangeBehavior;
+        //ThirdPersonInputHandler.OnPossessObject += selfRef.GetComponent<GuardController>().ChangeBehavior;
     }
 
     public override void StopBehavior()
     {
         base.StopBehavior();
-        ThirdPersonInputHandler.OnPossessObject -= selfRef.GetComponent<GuardController>().ChangeBehavior;
+        //ThirdPersonInputHandler.OnPossessObject -= selfRef.GetComponent<GuardController>().ChangeBehavior;
     }
 
     #endregion
@@ -60,12 +60,12 @@ public class ChaseBehavior : GuardMovement
     /// <returns></returns>
     private Vector3 GetPlayerLocation()
     {
-        return PlayerManager.Instance.PlayerGhostObject.transform.position;
+        return PlayerManager.Instance.CurrentObject.transform.position;
     }
 
     ~ChaseBehavior()
     {
-        if(selfRef != null)
-            ThirdPersonInputHandler.OnPossessObject -= selfRef.GetComponent<GuardController>().ChangeBehavior;
+        /*if(selfRef != null)
+            ThirdPersonInputHandler.OnPossessObject -= selfRef.GetComponent<GuardController>().ChangeBehavior;*/
     }
 }
