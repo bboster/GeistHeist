00c359e0563968d9f43db12a405971e98db57f15
jtkelly <59982037+jtkelly18@users.noreply.github.com>
2025-10-28 17:40:16 -0500

    Update VisionStimulus.cs

--- Geist Heist/Assets/Scripts/Guards/Pathing & Detection/VisionStimulus.cs
+++ Geist Heist/Assets/Scripts/Guards/Pathing & Detection/VisionStimulus.cs
@@ -50,10 +50,6 @@ public class VisionStimulus : Stimulus
 
     private void OnValidate()
     {
-        if (visionCollider == null)
-            visionCollider = GetComponent<Collider>();
-        if (spotLight == null)
-            spotLight = GetComponentInChildren<Light>();
         SyncLightToCollider();
     }
 
