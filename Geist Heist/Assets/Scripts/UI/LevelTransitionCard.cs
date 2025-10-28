7cefd239ce2ff9da31401d89c93d6440274967a7
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-10-28 09:34:28 -0500

    ok hi josh

--- Geist Heist/Assets/Scripts/UI/LevelTransitionCard.cs
+++ Geist Heist/Assets/Scripts/UI/LevelTransitionCard.cs
@@ -1,16 +1,31 @@
+/*
+ * Contributors: Toby
+ * Creation Date: 10/27/25
+ * Last Modified: 10/28/25
+ * 
+ * Brief Description: Brief animation that plays between levels
+ */
+
+using NaughtyAttributes;
+using System.Collections;
 using UnityEngine;
 
 public class LevelTransitionCard : MonoBehaviour
 {
+    [SerializeField] private float fadeInSeconds = 0.5f;
+    [SerializeField] private float waitingSeconds = 2;
+    [SerializeField] private float fadeOutSeconds = 0.5f;
+
+    [SerializeField, Required] CanvasGroup group;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void StartTransition(int sceneToLoad)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FadeIn()
     {
-        
+
     }
 }
