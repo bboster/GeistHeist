cede8d4b056f54704965e1a7ce143cfd4449318f
jtkelly <59982037+jtkelly18@users.noreply.github.com>
2025-10-28 17:33:20 -0500

    Flashlight Functional, needs spice.

--- Geist Heist/Assets/Scripts/Guards/Pathing & Detection/VisionStimulus.cs
+++ Geist Heist/Assets/Scripts/Guards/Pathing & Detection/VisionStimulus.cs
@@ -1,15 +1,16 @@
 /*
  * Author: Jacob Bateman
- * Contributors:
+ * Contributors: Joshua Kelly
  * Creation: 10/02/25
- * Last Edited: 10/07/25
+ * Last Edited: 10/28/25
  * Summary: Detects when the player enters or exits and enemy's vision cone and changes behavior accordingly.
  */
 
 using System.Collections;
-using UnityEngine;
 using GuardUtilities;
 using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
 
 public class VisionStimulus : Stimulus
 {
@@ -35,15 +36,30 @@ public class VisionStimulus : Stimulus
     [ShowIf("showProgrammingValues")]
     [SerializeField] private Transform raycastSpawn;
 
+    [ShowIf("showProgrammingValues")]
+    [SerializeField] private Light spotLight;
+    [ShowIf("showProgrammingValues")]
+    [SerializeField] private Collider visionCollider;
 
     [ShowIf("showProgrammingValues")]
     [SerializeField] private GuardController parentController;
 
     #endregion
 
+
+
+    private void OnValidate()
+    {
+        if (visionCollider == null)
+            visionCollider = GetComponent<Collider>();
+        if (spotLight == null)
+            spotLight = GetComponentInChildren<Light>();
+        SyncLightToCollider();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(timer != null)
+        if (timer != null)
         {
             StopCoroutine(timer);
             timer = null;
@@ -71,7 +87,7 @@ public class VisionStimulus : Stimulus
 
     private void OnTriggerExit(Collider other)
     {
-        if(other.gameObject.TryGetComponent(out PossessableObject obj))
+        if (other.gameObject.TryGetComponent(out PossessableObject obj))
         {
             if (obj.Equals(PlayerManager.Instance.PlayerGhostObject) && hasSeenPlayer == true)
             {
@@ -99,5 +115,36 @@ public class VisionStimulus : Stimulus
     public override void TriggerStimulus()
     {
         parentController.RecieveStimulus(this, stateToChangeTo);
+    }
+
+    [Button("Sync Light to Collider")]
+    private void SyncLightToCollider()
+    {
+        if (spotLight == null)
+        {
+            Debug.LogWarning("No spotlight assigned on VisionStimulus.");
+            return;
+        }
+
+        Collider col = GetComponent<Collider>();
+        if (col == null)
+        {
+            Debug.LogWarning("No collider found on VisionStimulus.");
+            return;
+        }
+
+        if (col is MeshCollider meshCol && meshCol.sharedMesh != null)
+        {
+            Bounds bounds = meshCol.sharedMesh.bounds;
+
+            float coneHeight = bounds.size.z * transform.localScale.z;
+            float coneRadius = Mathf.Max(bounds.size.x, bounds.size.y) * 0.5f * transform.localScale.x;
+
+            spotLight.range = coneHeight*5;
+            spotLight.spotAngle = Mathf.Rad2Deg * Mathf.Atan(coneRadius / coneHeight) * 2f;
+            spotLight.innerSpotAngle = spotLight.spotAngle * 0.8f;
+
+            Debug.Log($"[VisionStimulus] Synced spotlight from MeshCollider -> Range: {spotLight.range}, Angle: {spotLight.spotAngle}");
+        }
     }
 }
