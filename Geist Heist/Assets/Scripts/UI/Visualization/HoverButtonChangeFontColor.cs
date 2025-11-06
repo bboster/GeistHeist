65a48d32b43d6a8a6c0e23e6d3d7115f2ad519fb
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-11-06 11:25:03 -0600

    NEW FONTS AND AWESOMENESS

---
+++ Geist Heist/Assets/Scripts/UI/Visualization/HoverButtonChangeFontColor.cs
@@ -0,0 +1,61 @@
+/*
+ * Contributors: Toby Schamberger
+ * Creation: 11/6/25
+ * Last Edited: 11/6/25
+ * Summary: Change the color of a font text when user hover overs a selectable (button).
+ * Designed for vertical gradients rn. Can be updated to do multiple colors with an enum.
+ */
+
+using NaughtyAttributes;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class HoverButtonChangeFontColor : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField, Required] private Button button;
+    [SerializeField, Required] private TMP_Text targetText;
+
+    [Header("Unhovered color")]
+    [SerializeField] private Color unhoveredTopColor = Color.white;
+    [SerializeField] private Color unhoveredBottomColor = Color.white;
+
+    [Header("Hovered color")]
+    [SerializeField] private Color hoveredTopColor = Color.white;
+    [SerializeField] private Color hoveredBottomColor = Color.white;
+
+    void Start()
+    {
+        OnPointerExit(null);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        VertexGradient vg;
+        vg.topLeft = unhoveredTopColor;
+        vg.topRight = unhoveredTopColor;
+
+        vg.bottomLeft = unhoveredBottomColor;
+        vg.bottomRight = unhoveredBottomColor;
+        
+        targetText.colorGradient = vg;
+    }
+
+    void OnPointerEnter(PointerEventData data)
+    {
+        VertexGradient vg;
+        vg.topLeft = hoveredTopColor;
+        vg.topRight = hoveredTopColor;
+
+        vg.bottomLeft = hoveredBottomColor;
+        vg.bottomRight = hoveredBottomColor;
+
+        targetText.colorGradient = vg;
+    }
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        OnPointerEnter(eventData);
+    }
+}
