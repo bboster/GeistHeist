979f39040018e64e188e2a1c325126f99c624248
Veizyl <sturner@mail.bradley.edu>
2025-11-06 13:35:47 -0600

    Charge Meter Resets in Car

--- Geist Heist/Assets/Scripts/Player/Possession/ToyCar.cs
+++ Geist Heist/Assets/Scripts/Player/Possession/ToyCar.cs
@@ -139,6 +139,7 @@ public class ToyCar : IInputHandler
             UnFreezePosition();
             //for fixed update to handle physics better
             physicsEnabled = true;
+            StartCoroutine(ResetChargeMeter());
         }
     }
 
@@ -222,6 +223,12 @@ public class ToyCar : IInputHandler
     {
         rb.constraints = RigidbodyConstraints.None;
         rb.constraints = RigidbodyConstraints.FreezeRotation;
+    }
+
+    private IEnumerator ResetChargeMeter()
+    {
+        yield return new WaitForSeconds(0.1f);
+        currentStrength = minStrength;
     }
 
     private void OnDrawGizmos()
