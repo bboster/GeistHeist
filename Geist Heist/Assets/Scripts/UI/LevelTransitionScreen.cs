a080acae9cabb3a8b4e720cc62ff1f4aedb03b70
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-11-06 10:30:15 -0600

    push

--- Geist Heist/Assets/Scripts/UI/LevelTransitionScreen.cs
+++ Geist Heist/Assets/Scripts/UI/LevelTransitionScreen.cs
@@ -63,11 +63,11 @@ public class LevelTransitionScreen : MonoBehaviour
 
     private IEnumerator FadeIn()
     {
-        float startTime = Time.time;
+        float startTime = Time.unscaledTime;
         float time;
         do
         {
-            time = Time.time - startTime;
+            time = Time.unscaledTime - startTime;
             float t = time / fadeInSeconds;
 
             group.alpha = t;
@@ -79,11 +79,11 @@ public class LevelTransitionScreen : MonoBehaviour
 
     private IEnumerator FadeOut()
     {
-        float startTime = Time.time;
+        float startTime = Time.unscaledTime;
         float time;
         do
         {
-            time = Time.time - startTime;
+            time = Time.unscaledTime - startTime;
             float t = time / fadeInSeconds;
 
             group.alpha =  1- t;
