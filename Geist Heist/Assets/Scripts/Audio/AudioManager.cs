c452ef3e2bf2d54e51ea6cc571e57f9be7f1e7bb
Joseph Consoli <114947767+JRavioli@users.noreply.github.com>
2025-11-08 18:35:30 -0600

    changes so i can pull from working

---
+++ Geist Heist/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,74 @@
+using FMODUnity;
+using UnityEngine;
+using FMOD.Studio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance { get; private set; }
+
+    //Sets AudioManager instance in the scene
+    private void Awake()
+    {
+        
+        if (instance != null)
+        {
+            Debug.Log("There is more than one AudioManager in the scene");
+        }
+        instance = this;
+    }
+
+    //Plays a non-looping event WITHOUT 3d Attributes
+    public void PlayOneShot(EventReference sound)
+    {
+        RuntimeManager.PlayOneShot(sound);
+    }
+
+    //Plays a non-looping event WITH 3d Attributes (has a spacializer)
+    public void PlayOneShot(EventReference sound, Vector3 worldPos)
+    {
+        RuntimeManager.PlayOneShot(sound, worldPos);
+    }
+
+    public EventInstance CreateEventInstance(EventReference eventReference)
+    {
+        EventInstance eventInstance = RuntimeManager.CreateInstance(eventReference);
+        return eventInstance;
+    }
+
+    //Sets EventInstance e's 3d attributes to a gameObject's transform + rigidbody
+    //Should be called in e's update function
+    public static void SetEventParameters(ref EventInstance e, Transform t, Rigidbody r)
+    {
+        e.set3DAttributes(RuntimeUtils.To3DAttributes(t, r));
+    }
+
+    //Starts a looping sound effect from an ALREADY EXISTING INSTANCE
+    //Music will have it's own script and functions for transitions
+    //Not needed? Just play start function in each script
+    /*public void StartLoopingSFX(EventInstance sound)
+    {
+        sound.start();
+    }*/
+
+    //Stops a looping sound effect
+    //When fadeOut is true, allow for sound to finish
+    //When fadeOut is false, stop sound immediately
+    public void StopSFX(EventInstance sound, bool fadeOut)
+    {
+        if (fadeOut)
+        {
+            sound.stop(STOP_MODE.ALLOWFADEOUT);
+        }
+        else
+        {
+            sound.stop(STOP_MODE.IMMEDIATE);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        //TODO
+        //ADD A FADE EFFECT ON EVERY SOUND TO MAKE IT FADE OUT OVER A HALF SECOND INSTEAD OF CUTTING THE SHORT
+        //UNLESS MUSIC HAS SPECIAL TRANSITIONS BETWEEN SCENES, THEY SHOULD FOLLOW THE SAME RULE AS ABOVE
+    }
+}
---
+++ Geist Heist/Assets/Scripts/Audio/FMODEvents.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+using System.Collections.Generic;
+
+public class FMODEvents : MonoBehaviour
+{
+    [field: Header("Enemy SFX")]
+    [field: SerializeField] public EventReference GuardReactions { get; private set; }
+    [field: SerializeField] public EventReference GuardRun { get; private set; }
+    [field: SerializeField] public EventReference GuardWalk { get; private set; }
+    [field: SerializeField] public EventReference PlayerSpotted { get; private set; }
+
+    [field: Header("Environment SFX")]
+
+    [field: Header("Interface SFX")]
+    [field: SerializeField] public EventReference PossessionLow { get; private set; }
+    [field: SerializeField] public EventReference PossessionOut { get; private set; }
+    [field: SerializeField] public EventReference PossessionRefill { get; private set; }
+    
+    [field: Header("Interface SFX")]
+    [field: SerializeField] public EventReference PossessionEnter { get; private set; }
+    [field: SerializeField] public EventReference PossessionExit { get; private set; }
+
+
+    public static FMODEvents instance { get; private set; }
+    
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.Log("There is more than one FMODEvents in the scene");
+        }
+        instance = this;
+    }
+
+}
