b6ff20cbc3e7608cf3befb0e3d047c9077ded236
BBurtz <bburtz@mail.bradley.edu>
2025-11-17 14:21:43 -0600

    Made PA trigger and added the canvas needed to the dialogue manager

--- Geist Heist/Assets/Scripts/UI/DailougeManager.cs
+++ Geist Heist/Assets/Scripts/UI/DailougeManager.cs
@@ -1,15 +1,23 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class DailougeManager : Singleton<DailougeManager>
 {
     [SerializeField] private GameObject TextboxPrefab;
+    [SerializeField] private GameObject PAPrefab;
         
     public GameObject ResizingTextbox;
     public TMPro.TMP_Text Textbox;
+
+    public GameObject PAholder;
+    public TMPro.TMP_Text PATextbox;
 
     private void Start()
     {
         ResizingTextbox = Instantiate(TextboxPrefab);
         Textbox = ResizingTextbox.GetComponentInChildren<TMPro.TMP_Text>();
+
+        PAholder = Instantiate(PAPrefab);
+        PATextbox = PAholder.GetComponentInChildren<TMPro.TMP_Text>();
     }
 }
