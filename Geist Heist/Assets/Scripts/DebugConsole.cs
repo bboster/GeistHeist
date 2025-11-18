f7141e05e3b99b008275810ecccb0098933dd7ed
BBurtz <117851173+BBurtz@users.noreply.github.com>
2025-11-18 11:35:56 -0600

    Debug Console more commands working

--- Geist Heist/Assets/Scripts/DebugConsole.cs
+++ Geist Heist/Assets/Scripts/DebugConsole.cs
@@ -2,6 +2,7 @@ using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using UnityEngine.Windows;
 
@@ -11,6 +12,8 @@ public class DebugConsole : MonoBehaviour
     [SerializeField] TMPro.TMP_InputField inputs;
     [SerializeField] TMPro.TMP_Text TextArea;
     [SerializeField] GameObject Player;
+    [SerializeField] GameObject cameraGO;
+    [SerializeField] GameObject[] Prefabs;
 
     private bool noClipToggle = false;
     private bool godToggle = false;
@@ -22,6 +25,7 @@ public class DebugConsole : MonoBehaviour
         Console.SetActive(false);
         InputEvents.DebugStarted.AddListener(ToggleConsole);
         Player = FindFirstObjectByType<ThirdPersonInputHandler>().gameObject;
+        cameraGO = FindFirstObjectByType<Camera>().gameObject;
     }
 
 
@@ -59,7 +63,9 @@ public class DebugConsole : MonoBehaviour
         {
             Debug.Log("no clip");
             noClipToggle = !noClipToggle;
-            //make the play no clip
+            Player.GetComponent<CapsuleCollider>().enabled = !noClipToggle;
+            Player.GetComponentInChildren<SphereCollider>().enabled = !noClipToggle;
+            Player.GetComponent<Rigidbody>().useGravity = !noClipToggle;
             TextArea.text = TextArea.text + "\n" + Command + " " + noClipToggle;
         }
         else if(Command.ToLower() == "god")
@@ -71,28 +77,73 @@ public class DebugConsole : MonoBehaviour
         }
         else if(Command.ToLower() == "dc")
         {
+            //still haven't implemented
             Debug.Log("Detatch Camera");
             TextArea.text = TextArea.text + "\n" + Command + " " + cameraToggle;
         }
-        else if(Command.ToLower() == "ls")
+        else if(Command.ToLower().Substring(0,2) == "ls")
         {
+            //Still a little buggy
             Debug.Log("Load Scene");
+            int Temp;
+            if(int.TryParse(Command.Substring(3, Command.Length - 3), out Temp))
+            {
+                SceneManager.LoadScene(Temp);
+            }
+            else
+            {
+
+                SceneManager.LoadScene(Command.Substring(3, Command.Length - 3));
+            }
         }
         else if(Command.ToLower() == "freeze")
         {
+            //waiting for jacob to implement
             Debug.Log("Freeze");
             TextArea.text = TextArea.text + "\n" + Command + " " + freezeToggle;
         }
         else if(Command.ToLower() == "help")
         {
+            TextArea.text = TextArea.text + "\n" + Command + "\nNo Clip: nc \nGod Mode: god \nDetatch Camera: dc \nFreeze Guards: freeze " +
+                "\nLoad Scene: ls <Scene Name/Scnene Index> \nSpawn Item: spawn <Item Name/Item Index>";
             Debug.Log("Help");
         }
         else if(Command.Length > 4)
         {
             if (Command.ToLower().Substring(0, 5) == "spawn")
             {
+                int Temp;
+                string PostString = Command.Substring(6,Command.Length - 6);
                 Debug.Log("Spawn Item");
+                if (int.TryParse(PostString, out Temp))
+                {
+                    Instantiate(Prefabs[0], cameraGO.transform.position, Quaternion.identity);
+                }
+                else
+                {
+                    if(PostString.ToLower() == "vase")
+                    {
+                        Instantiate(Prefabs[0], cameraGO.transform.position, Quaternion.identity);
+                    }
+                    else if (PostString.ToLower() == "vending" || PostString.ToLower() == "vending machine")
+                    {
+                        Instantiate(Prefabs[1], cameraGO.transform.position, Quaternion.identity);
+                    }
+                    else if (PostString.ToLower() == "car")
+                    {
+                        Instantiate(Prefabs[2], cameraGO.transform.position, Quaternion.identity);
+                    }
+                }
+                Instantiate(Prefabs[0]);
             }
+            else if(Command.Length != 0)
+            {
+                TextArea.text = TextArea.text + "\n" + Command + " No command found, use Help for all commands";
+            }
+        }
+        else if(Command.Length != 0)
+        {
+            TextArea.text = TextArea.text + "\n" + Command + " No command found, use Help for all commands";
         }
         inputs.text = "";
         inputs.ActivateInputField();
