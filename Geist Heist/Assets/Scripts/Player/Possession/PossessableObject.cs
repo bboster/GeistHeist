7c77ccf74f187b09051de185aa59bc8c95a28901
SkyBeal <thesnugpug1@gmail.com>
2025-11-22 18:14:22 -0600

    should work!

--- Geist Heist/Assets/Scripts/Player/Possession/PossessableObject.cs
+++ Geist Heist/Assets/Scripts/Player/Possession/PossessableObject.cs
@@ -20,6 +20,7 @@ using System.Collections;
 using System.Collections.Generic;
 using FMOD.Studio;
 using FMODUnity;
+using GuardUtilities;
 
 public class PossessableObject : MonoBehaviour, IInteractable
 {
@@ -50,11 +51,14 @@ public class PossessableObject : MonoBehaviour, IInteractable
     [SerializeField, Required, ShowAssetPreview(16, 16), HideIf(nameof(isGhost))] public Material UnpossessedMaterial;
     [Tooltip("Material on possessable when it is used or when Ollie enters the possessable while a guard is in chase state.")]
     [SerializeField, Required, ShowAssetPreview(16, 16)] public Material VisiblePossessionMaterial;
+    [Tooltip("Material on possessable when a guard sees the possessable in chase state but possessable is NOT possessed.")]
+    [SerializeField, Required, ShowAssetPreview(16, 16)] public Material VisibleUnPossessedMaterial;
 
     [Header("Other")]
     [SerializeField] private bool isGhost = false;
 
     [HideInInspector] public bool CanUnPossess = true;
+    private bool possessionIsSafe = true;
     public IInputHandler InputHandler => GetInputHandler();
     private IInputHandler inputHandler;
 
@@ -69,7 +73,6 @@ public class PossessableObject : MonoBehaviour, IInteractable
     [HideInInspector] public bool PauseDischargeTimer = false;
 
     private EventInstance possessionEnter;
-
     private EventInstance possessionLow;
     private EventInstance possessionOut;
     private EventInstance possessionRefill;
@@ -80,6 +83,94 @@ public class PossessableObject : MonoBehaviour, IInteractable
 
     public static Action OnActionPerformed;
     public static Action OnObjectLeft;
+
+    private PlayerManager playerManager;
+
+    /// <summary>
+    /// using this for unsafe material changing
+    /// </summary>
+    /// <param name="other"></param>
+    private void OnTriggerEnter(Collider other)
+    {
+        //if interaction is vision cone
+        if (other.transform.GetComponent<VisionStimulus>() != null)
+        {
+            GuardController GC = other.transform.GetComponentInParent<GuardController>();
+
+            //if it is a guard in chase state + possessed
+            if (GC != null && GC.currentBehavior.StateName == GuardStates.chase && playerManager.CurrentObject != playerManager.PlayerGhostObject)
+            {
+                possessionIsSafe = false;
+                if (VisiblePossessionMaterial != null)
+                {
+                    this.meshRenderer.material = VisiblePossessionMaterial;
+                }
+            }
+
+            //if it is a guard in chase state + UNpossessed
+            else if (GC != null && GC.currentBehavior.StateName == GuardStates.chase && playerManager.CurrentObject == playerManager.PlayerGhostObject)
+            {
+                possessionIsSafe = false;
+                if (VisibleUnPossessedMaterial != null)
+                {
+                    this.meshRenderer.material = VisibleUnPossessedMaterial;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        //if interaction is vision cone
+        if (other.transform.GetComponent<VisionStimulus>() != null)
+        {
+            GuardController GC = other.transform.GetComponentInParent<GuardController>();
+
+            //if it is a guard in chase state + possessed
+            if (GC != null && GC.currentBehavior.StateName == GuardStates.chase && playerManager.CurrentObject != playerManager.PlayerGhostObject)
+            {
+                if (VisiblePossessionMaterial != null)
+                {
+                    this.meshRenderer.material = VisiblePossessionMaterial;
+                }
+            }
+
+            //if it is a guard in chase state + UNpossessed
+            else if (GC != null && GC.currentBehavior.StateName == GuardStates.chase && playerManager.CurrentObject == playerManager.PlayerGhostObject)
+            {
+                if (VisibleUnPossessedMaterial != null)
+                {
+                    this.meshRenderer.material = VisibleUnPossessedMaterial;
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if interaction is vision cone
+        if (other.transform.GetComponent<VisionStimulus>() != null)
+        {
+            //if player is still inside
+            if (playerManager.CurrentObject != playerManager.PlayerGhostObject)
+            {
+                possessionIsSafe = true;
+                if (PossessedMaterial != null)
+                {
+                    this.meshRenderer.material = PossessedMaterial;
+                }
+            }
+            //is player is not possessing
+            else if (playerManager.CurrentObject == playerManager.PlayerGhostObject)
+            {
+                possessionIsSafe = true;
+                if (UnpossessedMaterial != null)
+                {
+                    this.meshRenderer.material = UnpossessedMaterial;
+                }
+            }
+        }
+    }
 
     #endregion
 
@@ -92,6 +183,7 @@ public class PossessableObject : MonoBehaviour, IInteractable
 
     void Start()
     {
+        playerManager = FindObjectOfType<PlayerManager>();
         currentTimerPercentage = maxChargePercentage;
 
         if (ghostExitPoints.Count == 0)
@@ -152,7 +244,7 @@ public class PossessableObject : MonoBehaviour, IInteractable
         gameObject.SetActive(true);
         InputHandler.OnPossessionStart();
 
-        if(PossessedMaterial != null)
+        if(PossessedMaterial != null && possessionIsSafe)
             meshRenderer.material = PossessedMaterial;
         else
             Debug.LogWarning("No possession material for "+gameObject.name);
@@ -186,7 +278,7 @@ public class PossessableObject : MonoBehaviour, IInteractable
             return;
         }
 
-        if (PossessedMaterial != null)
+        if (UnpossessedMaterial != null && possessionIsSafe)
             meshRenderer.material = UnpossessedMaterial;
         else
             Debug.LogWarning("No unpossession material for " + gameObject.name);
