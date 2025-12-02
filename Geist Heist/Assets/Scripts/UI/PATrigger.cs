5f76a6941ee1e7a466d0f5315993b1d7d2210927
Joseph Consoli <114947767+JRavioli@users.noreply.github.com>
2025-12-02 11:01:24 -0600

    Revert "Car + PA Trigger save"
    This reverts commit ec2c21ee065c235fd10daa92f4c9eb2f675c32ab.

--- Geist Heist/Assets/Scripts/UI/PATrigger.cs
+++ Geist Heist/Assets/Scripts/UI/PATrigger.cs
@@ -20,8 +20,6 @@ public class PATrigger : MonoBehaviour
         if (other.gameObject.GetComponent<ThirdPersonInputHandler>() != null && !alreadyTriggered)
         {
             //play audio clip here joey
-            AudioManager.Instance.PlayOneShot(FMODEvents.instance.PAJingle);
-
             alreadyTriggered = true;
             DialougeManager.Instance.DisplayText_PASystem(Text, stayLength);
         }
