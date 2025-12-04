df0557f8e2611d64891ea28d05ab0a40e548a915
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2025-12-04 11:31:36 -0600

    Everything could have been lost

--- Geist Heist/Assets/Scripts/UI/MainMenu.cs
+++ Geist Heist/Assets/Scripts/UI/MainMenu.cs
@@ -103,6 +103,8 @@ public class MainMenu : MonoBehaviour
     # region Main Page
     void OnNewGameButtonClicked()
     {
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
         if (!playerHasSignificantSaveData)
         {
             LoadNewGame();
@@ -118,27 +120,35 @@ public class MainMenu : MonoBehaviour
 
     void OnContinueButtonClicked()
     {
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
         //SceneManager.LoadScene(HubScene);
         LoadScene(HubScene, NewSceneLoadingCardPrefab);
     }
 
     void OnCreditsButtonClicked()
     {
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
         StaticUtilities.DisableCanvasGroup(howToPlayPage);
         StaticUtilities.EnableCanvasGroup(creditsPage);
     }
 
     void OnHowToPlayButtonClicked()
     {
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
         StaticUtilities.DisableCanvasGroup(creditsPage);
         StaticUtilities.EnableCanvasGroup(howToPlayPage);
     }
 
     void OnQuitButtonClicked()
     {
-    #if UNITY_EDITOR
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
+#if UNITY_EDITOR
         EditorApplication.isPlaying = false;
-    #else
+#else
         Application.Quit();
     #endif
     }
@@ -149,6 +159,8 @@ public class MainMenu : MonoBehaviour
 
     void OnConfirmDeleteSaveButtonClicked()
     {
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
         LoadNewGame();
     }
 
@@ -158,6 +170,8 @@ public class MainMenu : MonoBehaviour
 
     void OnCreditsBackButtonClicked()
     {
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
         StaticUtilities.DisableCanvasGroup(creditsPage);
     }
 
@@ -167,6 +181,8 @@ public class MainMenu : MonoBehaviour
 
     void OnCloseHowToPlayButtonClicked()
     {
+        AudioManager.Instance.PlayOneShot(FMODEvents.instance.UIClick);
+
         StaticUtilities.DisableCanvasGroup(howToPlayPage);
     }
 
