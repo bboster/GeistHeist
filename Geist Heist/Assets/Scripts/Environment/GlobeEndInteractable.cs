701da28eeef9af624131a4b9259d7b2972611862
SkyBeal <thesnugpug1@gmail.com>
2026-02-10 11:45:47 -0600

    wip

---
+++ Geist Heist/Assets/Scripts/Environment/GlobeEndInteractable.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using UnityEngine;
+
+public class GlobeEndInteractable : MonoBehaviour, IActionable
+{
+    [SerializeField] private int endingButtonPresses = 10;
+    private Coroutine endCoroutine;
+
+    public void Action()
+    {
+        Debug.Log("test");
+
+        if (endCoroutine == null)
+        {
+            endCoroutine = StartCoroutine(ButtonPressMinigame());
+        }
+    }
+
+    public IEnumerator ButtonPressMinigame()
+    {
+        while (true)
+        {
+            if (endingButtonPresses <= 0)
+            {
+                //initiate ending cutscene
+            }
+
+            yield return null;
+        }
+    }
+
+    /*
+    bool IsActionable()
+    {
+        if (SaveDataManager.Instance.GetLevelsCompletedCount() == )
+    }
+    */
+}
---
+++ Geist Heist/Assets/Scripts/Player/Possession/GlobePossessable.cs
@@ -0,0 +1,91 @@
+using NaughtyAttributes;
+using System.Collections;
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GlobePossessable : IInputHandler
+{
+    [SerializeField] private CinemachineCamera globeCamera;
+    private PossessableObject possessableObject => GetComponent<PossessableObject>();
+
+    private Coroutine globePossessAnimation;
+    [SerializeField, Scene] private string GlobeScene = "ACTUAL Globe Scene";
+
+    public override void WhilePossessingUpdate()
+    {
+    }
+
+    public override void OnPossessionStart()
+    {
+        if (globePossessAnimation != null)
+        {
+            //globePossessAnimation = StartCoroutine(LoadNextSceneCooldown());
+        }
+    }
+
+    public override void OnPossessionEnded()
+    {
+    }
+
+
+    #region action
+    public override void OnActionStarted()
+    {
+        Debug.Log("please");
+    }
+
+    public override void WhileActionHeld(float secondsHeld)
+    {
+    }
+
+    public override void WhileActionNotHeld(float secondsNotHeld)
+    {
+    }
+
+    public override void OnActionCanceled(float secondsHeld)
+    {
+    }
+
+    #endregion
+
+    #region Possess
+    public override void OnInteractStarted()
+    {
+    }
+
+    public override void WhileInteractHeld(float secondsHeld)
+    { }
+
+    public override void OnInteractCanceled(float secondsHeld)
+    {
+    }
+    #endregion
+
+    #region Move
+    public override void OnMoveStarted()
+    {
+
+    }
+    public override void WhileMoveHeld(float secondsHeld)
+    {
+    }
+
+    public override void WhileMoveNotHeld()
+    {
+    }
+    public override void OnMoveCanceled(float secondsHeld) { }
+
+
+    #endregion
+
+    //TO DO: adapt this to start on the tether and maybe go to another script when we get an animation, currently changes scenes abruptly
+    IEnumerator LoadNextSceneCooldown()
+    {
+
+        yield return new WaitForSeconds(1.5f);
+
+        //SceneManager.LoadScene(HubScene);
+        LevelManager.Instance.ChangeScene(GlobeScene);
+    }
+}
