6e4a03e3159ec6106cb7e2bfe4d7b8aef40d512f
SkyBeal <thesnugpug1@gmail.com>
2026-02-12 09:52:13 -0600

    ui

--- Geist Heist/Assets/Scripts/Environment/GlobeEndActionable.cs
+++ Geist Heist/Assets/Scripts/Environment/GlobeEndActionable.cs
@@ -1,14 +1,21 @@
 using System.Collections;
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class GlobeEndActionable : MonoBehaviour, IActionable
 {
+    [SerializeField] private CinemachineCamera globeCam;
     [SerializeField] private int endingButtonPresses = 10;
     private Coroutine endCoroutine;
 
     public void Action()
     {
         Debug.Log("test");
+
+        if (globeCam.Priority == 0)
+        {
+            globeCam.Priority++;
+        }
 
         if (endCoroutine == null)
         {
