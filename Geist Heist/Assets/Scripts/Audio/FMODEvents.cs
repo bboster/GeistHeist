bd5d9fa391d3d7870abe0313af7ee3ba3a251244
Joseph Consoli <114947767+JRavioli@users.noreply.github.com>
2026-03-24 11:21:34 -0500

    Build Again

--- Geist Heist/Assets/Scripts/Audio/FMODEvents.cs
+++ Geist Heist/Assets/Scripts/Audio/FMODEvents.cs
@@ -21,7 +21,6 @@ public class FMODEvents : Singleton<FMODEvents>
 
     [field: Header("Environment SFX")]
     [field: SerializeField] public EventReference PAJingle { get; private set; }
-    [field: SerializeField] public EventReference PALines { get; private set; }
 
     [field: Header("Interface SFX")]
     [field: SerializeField] public EventReference PossessionLow { get; private set; }
@@ -43,8 +42,12 @@ public class FMODEvents : Singleton<FMODEvents>
     [field: SerializeField] public EventReference CarWind { get; private set; }
     [field: SerializeField] public EventReference CarGo { get; private set; }
     [field: SerializeField] public EventReference CarBump { get; private set; }
-
-    [field: Header("Prop SFX")]
     [field: SerializeField] public EventReference DoorOpen { get; private set; }
     [field: SerializeField] public EventReference DoorLocked { get; private set; }
+
+    [field: Header("Voicelines")]
+    [field: SerializeField] public EventReference VLTutorial { get; private set; }
+    [field: SerializeField] public EventReference VLLobbyNightOne { get; private set; }
+    [field: SerializeField] public EventReference VLParlor { get; private set; }
+    [field: SerializeField] public EventReference VLLobbyNightTwo { get; private set; }
 }
