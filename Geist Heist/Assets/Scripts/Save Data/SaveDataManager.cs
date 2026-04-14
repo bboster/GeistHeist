0d02eb58049a45ccbd7d9efef6adf6d1e2025b96
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2026-04-14 09:33:37 -0500

    Save Data

--- Geist Heist/Assets/Scripts/Save Data/SaveDataManager.cs
+++ Geist Heist/Assets/Scripts/Save Data/SaveDataManager.cs
@@ -178,7 +178,25 @@ public class SaveDataManager : DontDestroyOnLoadSingleton<SaveDataManager>
             SaveData();
     }
 
+    #endregion
+
+    #region Toy Car
+
+    public bool HasPlayerMovedWithCar()
+    {
+        EnsureSaveData();
+        return currentSaveDta.HasPlayerMovedWithCar;
+    }
+
+    public void SetHasPlayerMovedWithCar(bool hasDriven, bool autoSave = true)
+    {
+        EnsureSaveData();
+        currentSaveDta.HasPlayerMovedWithCar = hasDriven;
+        if (autoSave) SaveData();
+    }
+
     #endregion 
+
 
     #region File Manipulation
 
