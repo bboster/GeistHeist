54d74a241947086d79c2e5460445ea690ceb6f8e
Toby <104741505+TheBeesSneeze@users.noreply.github.com>
2026-04-16 10:19:12 -0500

    Whyyy

--- Geist Heist/Assets/Scripts/UI/MainMenu.cs
+++ Geist Heist/Assets/Scripts/UI/MainMenu.cs
@@ -35,7 +35,6 @@ public class MainMenu : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField, Required] private ConfirmationPopup confirmationPopup;
-    [SerializeField, Required] private GameObject loadingScreenPrefab;
 
     [Header("Fog")]
     [SerializeField, Required] private RenderTexture leftFogRenderTexture;
@@ -205,16 +204,10 @@ public class MainMenu : MonoBehaviour
 
     void LoadScene(string sceneToLoad, GameObject loadingCardPrefab)
     {
-        if (loadingScreenPrefab == null)
-        {
-            Debug.LogError("No transition card set on " + gameObject.name);
-            LevelManager.Instance.ChangeScene(sceneToLoad);
-            return;
-        }
-        var levelTransition = Instantiate(loadingScreenPrefab).GetComponent<LevelTransitionScreen>();
-        levelTransition.StartTransition(sceneToLoad, loadingCardPrefab);
+        Debug.LogError("No transition card set on " + gameObject.name);
+        //SceneLoadManager.Instance.LoadScene(sceneToLoad);
+        LevelManager.Instance.InstantiateFadeToBlack(() => LevelManager.Instance.ChangeScene(HubScene));
     }
-
 
     #region Buttons OnClicked
 
