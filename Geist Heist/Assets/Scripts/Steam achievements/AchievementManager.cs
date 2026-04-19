d19a09f00685166d26465dfe3e3940663c49e842
BBurtz <117851173+BBurtz@users.noreply.github.com>
2026-04-19 15:32:29 -0500

    removed the 2 problem achievements

--- Geist Heist/Assets/Scripts/Steam achievements/AchievementManager.cs
+++ Geist Heist/Assets/Scripts/Steam achievements/AchievementManager.cs
@@ -8,7 +8,7 @@ public class AchievementManager : Singleton<AchievementManager>
     private bool connectedToSteam = false;
     private int totalNumberOfAchievements = 30;
 
-    public enum eAchievements {OpenGame, Credits, BeatTutorial, BeatLevel1, BeatLevel2, BeatLevel3, BeatLevel4, BeatLevel5, InteractWithLore, RollOut, FinishGame, OpenDoor, OpenPeachDoor, CollectFirstHat, TryOnHat, TryOnLobster, TryOnSleepy, TryOnJester, CollecttwelveHats, CollectAllHats, PosessVase20, throwCan, ThrowCan20, Drive, Bonk5, Drive20, getcaught, CanGuard, CarGuard3, EscapeGuard }
+    public enum eAchievements {OpenGame, Credits, BeatTutorial, BeatLevel1, BeatLevel2, BeatLevel3, BeatLevel4, BeatLevel5, InteractWithLore, RollOut, FinishGame, OpenDoor, OpenPeachDoor, CollectFirstHat, TryOnHat, TryOnLobster, TryOnSleepy, TryOnJester, CollecttwelveHats, CollectAllHats, PosessVase20, throwCan, ThrowCan20, Drive, Bonk5, Drive20, getcaught, EscapeGuard }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
